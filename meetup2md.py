7a37087debc094f23a4bd56abd12c78d59a40214
unintendedbear <unintendedbear@gmail.com>
2016-10-31 15:56:17 +0100

    Ya crea el post con la cabecera para que se vaya a la pestaña de eventos. References #2

--- meetup2md.py
+++ meetup2md.py
@@ -3,22 +3,52 @@
 """meetup2md.py"""
 
 import json
+import requests
 import sys
 import re
+import time
 
 theAPIKey = ""
-theEvent = "234640453"
 
-if len(sys.argv) == 2:
-    args = sys.argv
-    theEventURL = args[1]
-    pattern = r'h?t?t?p?s?:?/*www.meetup.com/es-ES/([A-Za-z-]*)/events/(\d+)/'
-    matchURL = re.match(pattern, theEventURL)
-    if matchURL:
-        theGroup = matchURL.group(1)
-        theEvent = matchURL.group(2)
-else:
-    print "Usa el método así: python meetup2md.py urldelevento"
-    sys.exit()
+def main():
 
-theURL = "https://api.meetup.com/"+theGroup+"/events/"+theEvent+"?&sign=true&photo-host=public&key="+theAPIKey
+    # Building the request
+    if len(sys.argv) == 2:
+        args = sys.argv
+        theEventURL = args[1]
+        pattern = r'h?t?t?p?s?:?/*www.meetup.com/es-ES/([A-Za-z-]*)/events/(\d+)/'
+        matchURL = re.match(pattern, theEventURL)
+        if matchURL:
+            theGroup = matchURL.group(1)
+            theEvent = matchURL.group(2)
+        else:
+            print ("Asegúrate de que la URL está bien escrita")
+            sys.exit()
+    else:
+        print ("Usa el método así: python meetup2md.py urldelevento")
+        sys.exit()
+
+    theURL = "https://api.meetup.com/"+theGroup+"/events/"+theEvent+"?&sign=true&photo-host=public&key="+theAPIKey
+
+    # Obtaining the response in JSON
+    theJSON = obtain_json(theURL)
+
+    # Preparing the output file, a markdown post
+    location = "_posts/"
+    date = time.strftime("%Y-%m-%d")
+    postName = re.sub(r"\s+", '-', theJSON['name'])
+    postFilename = location+date+"-"+postName+".markdown"
+
+    # Writing the post
+    post = open(postFilename, 'w')
+    post.write("---\nlayout: post\ntitle: "+theJSON['name']+"\ndate: "+time.strftime("%Y-%m-%d %H:%M:%S")+"\ncategories: [\"eventos\"]\nauthor: \"Geek & Tech Girls\"\n---")
+    post.close()
+
+def obtain_json(eventURL):
+    request = requests.get(eventURL)
+    data = request.json()
+
+    return data
+
+if __name__=="__main__":
+        main()
