db80934c365cad43746ee0733c1fb6926ff6a9c0
unintendedbear <unintendedbear@gmail.com>
2016-10-31 14:36:09 +0100

    Se obtiene el nombre del grupo y el número de evento para hacer la consulta. Relates #2

--- meetup2md.py
+++ meetup2md.py
@@ -12,12 +12,13 @@ theEvent = "234640453"
 if len(sys.argv) == 2:
     args = sys.argv
     theEventURL = args[1]
-    pattern = r'https?://www.meetup.com/es-ES/Granada-Geek/events/(\d+)/'
+    pattern = r'h?t?t?p?s?:?/*www.meetup.com/es-ES/([A-Za-z-]*)/events/(\d+)/'
     matchURL = re.match(pattern, theEventURL)
     if matchURL:
-        print "El evento es: ", matchURL.group(1)
+        theGroup = matchURL.group(1)
+        theEvent = matchURL.group(2)
 else:
     print "Usa el método así: python meetup2md.py urldelevento"
     sys.exit()
 
-theURL = "https://api.meetup.com/Granada-Geek/events/"+theEvent+"?&sign=true&photo-host=public&key="+theAPIKey
+theURL = "https://api.meetup.com/"+theGroup+"/events/"+theEvent+"?&sign=true&photo-host=public&key="+theAPIKey
